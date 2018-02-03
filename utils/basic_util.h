ff13f61f68fca57446d87b5ee1e7199ed4ff28a2
yedushusheng <yedushusheng@163.com>
2018-02-03 22:25:21 +0800

    define data type of int

--- utils/basic_util.h
+++ utils/basic_util.h
@@ -2,3 +2,9 @@
 #define RTCODE_ERROR -1
 #define RTCODE_SUCCESS 0
 #define RTCODE_PARAM_ERROR -1
+
+/*define the data type*/
+typedef int32_t int;
+typedef uint32_t unsigned int;
+typedef int64_t long int;
+typedef uint64_t unsigned long int;
