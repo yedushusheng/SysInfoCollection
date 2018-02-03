dc3c167abe542c6bb1007533442d4ec69400ddff
yedushusheng <yedushusheng@163.com>
2018-02-03 22:32:07 +0800

    define different level of log function

---
+++ utils/log_util.c
@@ -0,0 +1,18 @@
+#include"log_util.h"
+
+void log_info(){
+
+}
+
+void log_warning(){
+
+}
+
+void log_error(){
+
+}
+
+void log_fatal(){
+
+}
+
---
+++ utils/log_util.h
@@ -0,0 +1,7 @@
+#ifndef _LOG_UTIL_H_
+#define _LOG_UTIL_H_
+
+void log_info();
+void log_warning();
+void log_error();
+void log_fatal();
