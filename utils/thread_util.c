ba3b8717032b5acf18d9a8c903acd9353a9bc510
yedushusheng <yedushusheng@163.com>
2018-02-03 23:11:49 +0800

    modify return value

--- utils/thread_util.c
+++ utils/thread_util.c
@@ -7,25 +7,25 @@
 int32_t thread_mutex_init(thread_mutex_t * mutex, thread_mutexattr_t * attr){
 	int32_t ret = RTCODE_SUCCESS;
 	ret = pthread_mutex_init((pthread_mutex_t*)mutex,(pthread_mutexattr_t*)attr);
-	if(RTCODE_SUCCESS != ret){
+	if(RTCODE_ERROR == ret){
 		log_error("init thread mutex failed,ret=%d,err='%m'",ret);
 	}
 	return ret;
 }
 
 int32_t thread_mutex_lock(thread_mutex_t * mutex){
-	int32_t ret = 0;
+	int32_t ret = RTCODE_SUCCESS;
 	ret = pthread_mutex_lock((pthread_mutex_t*)mutex);
-	if(ret != 0){
+	if(RTCODE_ERROR == ret){
 		log_error("get mutex failed,return=%d,errno='%m'",ret);
 	}
 	return ret;
 }
 
 int32_t thread_mutex_unlock(thread_mutex_t * mutex){
-	int32_t ret = 0;
+	int32_t ret = RTCODE_SUCCESS;
 	ret = pthread_mutex_unlock((pthread_mutex_t*)mutex);
-	if(ret != 0){
+	if(RTCODE_ERROR == ret){
 		log_error("get mutex failed,return=%d,errno='%m'",ret);
 	}
 	return ret;
