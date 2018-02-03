6d8b28f34d4155f5862ae8a4d19e3883dde57f40
yedushusheng <yedushusheng@163.com>
2018-02-03 23:00:49 +0800

    implement thread_mutex_init function

--- utils/thread_util.c
+++ utils/thread_util.c
@@ -4,6 +4,15 @@
 
 #include"thread_util.h"
 
+int32_t thread_mutex_init(thread_mutex_t * mutex, thread_mutexattr_t * attr){
+	int32_t ret = RTCODE_SUCCESS;
+	ret = pthread_mutex_init((pthread_mutex_t*)mutex,(pthread_mutexattr_t*)attr);
+	if(RTCODE_SUCCESS != ret){
+		log_error("init thread mutex failed,ret=%d,err='%m'",ret);
+	}
+	return ret;
+}
+
 int32_t thread_mutex_lock(thread_mutex_t * mutex){
 	int32_t ret = 0;
 	ret = pthread_mutex_lock((pthread_mutex_t*)mutex);
