ea59b728804a65d69e95c495fac0185a922df3ef
yedushusheng <yedushusheng@163.com>
2018-02-06 22:15:16 +0800

    implement kv_get_int32 function

--- utils/kv_util.c
+++ utils/kv_util.c
@@ -1,10 +1,45 @@
 #include"kv_util.h"
 
 kv_data_t *kv_new(){
-	return g_key)file_new();
+	return g_key_file_new();
 }
 
 int32_t kv_free(kv_data_t * kvdata){
 	g_key_file_free(kvdata);
 	return RTCODE_SUCCESS;
+}
+
+int32_t kv_get_int32(kv_data_t *kvdata,const char *domain,const char *key,int32_t *value){
+	GError *error = NULL;
+	gchar* var = 0;
+
+	check_null(kvdata,RTCODE_ERROR);
+	check_null(domain,RTCODE_ERROR);
+	check_null(key,RTCODE_ERROR);
+
+	var = g_key_file_get_value(kvdata,domain,key,&error);
+	if(error){
+		log_error("get int32 kv failed,err=%s,code=%d",error>message,error>code);
+		g_error_free(error);
+		return RTCODE_ERROR;
+	}
+
+	if(0 == strcmp(var,"")){
+		g_free(var);
+	}
+	(*value) = atoi(var);
+
+	return RTCODE_SUCCESS;
+}
+
+int32_t kv_get_int64(kv_data_t *kvdata,const char *domain,const char *key,int64_t *value){
+
+}
+
+int32_t kv_get_uint32(kv_data_t *kvdata,const char *domain,const char *key,uint32_t *value){
+
+}
+
+int32_t kv_get_uint64(kv_data_t *kvdata,const char *domain,const char *key,uint64_t *value){
+
 }
