271641e4f50e69c93e869eab09bf082efb2d0c0c
yedushusheng <yedushusheng@163.com>
2018-02-06 21:54:44 +0800

    define various kv_get_int function

--- utils/kv_util.h
+++ utils/kv_util.h
@@ -8,3 +8,7 @@ typedef GKeyFile kv_data_t;
 
 kv_data_t *kv_new();
 int32_t kv_free(kv_data_t *kvdata);
+int32_t kv_get_int32(kv_data_t *kvdata,const char *domain,const char *key,int32_t *value);
+int32_t kv_get_int64(kv_data_t *kvdata,const char *domain,const char *key,int64_t *value);
+int32_t kv_get_uint32(kv_data_t *kvdata,const char *domain,const char *key,uint32_t *value);
+int32_t kv_get_uint64(kv_data_t *kvdata,const char *domain,const char *key,uint64_t *value);
