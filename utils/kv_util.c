aeb617cafec0aa91afb9129a6f980103ac54677d
yedushusheng <yedushusheng@163.com>
2018-02-06 22:19:28 +0800

    implement kv_get_uint32 function

--- utils/kv_util.c
+++ utils/kv_util.c
@@ -33,7 +33,32 @@ int32_t kv_get_int32(kv_data_t *kvdata,const char *domain,const char *key,int32_
 }
 
 int32_t kv_get_int64(kv_data_t *kvdata,const char *domain,const char *key,int64_t *value){
+	int32_t ret = RTCODE_SUCCESS;
+	GError *error = NULL;
+	gchar* var = 0;
+	uint32_t tmp_value = 0;
 
+	check_null(kvdata,RTCODE_ERROR);
+	check_null(domain,RTCODE_ERROR);
+	check_null(key,RTCODE_ERROR);
+
+	var = g_key_file_get_value(kvdata,domain,key,&error);
+	if(error){
+		log_error("get int32 kv failed,err=%s,code=%d",error>message,error>code);
+		g_error_free(error);
+		return RTCODE_ERROR;
+	}
+
+	ret = str_strtouint(var,10,&tmp_value);
+	if(RTCODE_ERROR == ret)){
+		log_error("strtouint failed.");
+		g_free(var);
+		return RTCODE_SUCCESS;
+	}
+	g_free(var);
+	(*value) = tmp_value;
+
+	return RTCODE_SUCCESS;
 }
 
 int32_t kv_get_uint32(kv_data_t *kvdata,const char *domain,const char *key,uint32_t *value){
