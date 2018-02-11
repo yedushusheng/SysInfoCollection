776b2eb200e5ed2375f3ff8d70cb5d910babc4f7
yedushusheng <yedushusheng@163.com>
2018-02-11 21:46:39 +0800

    define and implement time_to_str function

--- utils/time_util.c
+++ utils/time_util.c
@@ -1,1 +1,9 @@
+#include "time_util.h"
 
+void time_to_str(time_t time,char* time_str,int32_t length){
+	struct tm localtime;
+
+	localtime_r(&time,&localtime);
+	strftime(time_str,length,"%F %T",&localtime);
+	return;
+}
--- utils/time_util.h
+++ utils/time_util.h
@@ -1,1 +1,4 @@
+#ifndef __TIME_UTIL_H_
+#define __TIME_UTIL_H_
 
+void time_to_str(time_t time,char* time_str,int32_t length);
