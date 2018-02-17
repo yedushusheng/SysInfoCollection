2867b782db1ea5852746da48dcd9efa7ca70613c
yedushusheng <yedushusheng@163.com>
2018-02-17 22:31:30 +0800

    add extern null_uuid

--- utils/uuid_util.h
+++ utils/uuid_util.h
@@ -2,7 +2,13 @@
 #define __UUID_UTIL_H__
 
 #include "basic_util.h"
+#include "log_util.h"
+
 int32_t uuid_key_init();
 char *uuid_unparse(uuid_t uuid);
 int32_t uuid_cpmpare(uuid_t uuid1,uuid_t uuid2);
 int32_t uuid_str_is_valid(char *uuid_str);
+
+extern uuid_t null_uuid;
+
+#endif
