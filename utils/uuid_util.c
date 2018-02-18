2d84046c495435f53dad8447e2f57024a67470d3
yedushusheng <yedushusheng@163.com>
2018-02-18 22:04:09 +0800

    implement uuid_cpmpare function

--- utils/uuid_util.c
+++ utils/uuid_util.c
@@ -3,3 +3,12 @@
 uuid_t null_uuid;
 uuid_t one_uuid;
 static pthread_key_t uuid_log_key;
+
+int32_t uuid_cpmpare(uuid_t uuid1, uuid_t uuid2){
+	int32_t i = 0;
+	for(i=0;i<4;i++){
+		if(((int32_t *)uuid1)[i]!=((int32_t *)uuid2)[i])
+			return RTCODE_ERROR;
+	}
+	return RTCODE_SUCCESS;
+}
