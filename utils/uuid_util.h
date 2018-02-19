9bf9c225080b3663d04a662805db4b2e4137804c
yedushusheng <yedushusheng@163.com>
2018-02-19 19:15:19 +0800

    define uuid_key_destroy function

--- utils/uuid_util.h
+++ utils/uuid_util.h
@@ -5,6 +5,7 @@
 #include "log_util.h"
 
 int32_t uuid_key_init();
+void uuid_key_destroy(void *pointer);
 char *uuid_unparse(uuid_t uuid);
 int32_t uuid_cpmpare(uuid_t uuid1,uuid_t uuid2);
 int32_t uuid_str_is_valid(char *uuid_str);
