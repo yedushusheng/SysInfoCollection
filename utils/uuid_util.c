16b2c582e4eb7fcd5bbb079b1658709c295c50a1
yedushusheng <yedushusheng@163.com>
2018-02-19 19:21:17 +0800

    add description of uuid_key_destroy function

--- utils/uuid_util.c
+++ utils/uuid_util.c
@@ -4,6 +4,14 @@ uuid_t null_uuid;
 uuid_t one_uuid;
 static pthread_key_t uuid_log_key;
 
+/****************************************
+* 函数名称: uuid_key_destroy
+* 函数功能: 释放键key指针内存
+* 入     参: void指针pointer
+* 出     参: 无
+* 返 回 值: 无
+* 备     注: 无
+****************************************/
 void uuid_key_destroy(void * pointer){
 	if(NULL != pointer){
 		free(pointer);
