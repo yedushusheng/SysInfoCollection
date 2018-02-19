441ecec5d01dfbda24ab137b5a6053d5ac29a4bb
yedushusheng <yedushusheng@163.com>
2018-02-19 18:48:26 +0800

    modift uuid_cpmpare return code

--- utils/uuid_util.c
+++ utils/uuid_util.c
@@ -5,10 +5,13 @@ uuid_t one_uuid;
 static pthread_key_t uuid_log_key;
 
 int32_t uuid_cpmpare(uuid_t uuid1, uuid_t uuid2){
+	int32_t ret = RTCODE_SUCCESS;
 	int32_t i = 0;
 	for(i=0;i<4;i++){
 		if(((int32_t *)uuid1)[i]!=((int32_t *)uuid2)[i])
-			return RTCODE_ERROR;
+			ret = RTCODE_ERROR;
+		else
+			ret = RTCODE_SUCCESS;
 	}
-	return RTCODE_SUCCESS;
+	return ret;
 }
