511447100560b1b9453ef7e9d1873ffa30a8fca4
yedushusheng <yedushusheng@163.com>
2018-02-22 23:53:34 +0800

    define net_accept function

--- utils/net_util.h
+++ utils/net_util.h
@@ -6,3 +6,4 @@
 
 int32_t net_socket(int32_t family,int32_t type,int32_t protocol);
 int32_t net_listen(int sock_fd,int32_t back_log);
+int32_t net_accept(int sock_fd,struct sockaddr *addr,socklen_t *addr_len);
