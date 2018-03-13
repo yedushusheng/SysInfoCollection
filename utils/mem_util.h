e0489476b256194b9313f1f27e08dc8f50567cfc
yedushusheng <yedushusheng@163.com>
2018-03-13 23:23:23 +0800

    增加分配内存的宏定义mem_malloc

--- utils/mem_util.h
+++ utils/mem_util.h
@@ -1,6 +1,12 @@
 #ifndef __MEM_UTIL_H_
 #define __MEM_UTIL_H_
 
+#define mem_malloc(mem_size){	\
+	do{					\
+		malloc(mem_size);		\
+	}					\
+}
+
 #define mem_free(ptr){	\
 	do{					\
 		if(NULL!=ptr){	\
