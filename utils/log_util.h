2f2753d3806794dd559e4ac98bc6bf1386c50520
yedushusheng <yedushusheng@163.com>
2018-03-16 23:20:32 +0800

    采用宏定义设置日志打印

--- utils/log_util.h
+++ utils/log_util.h
@@ -1,7 +1,12 @@
 #ifndef _LOG_UTIL_H_
 #define _LOG_UTIL_H_
 
-void log_info();
+#define log_info(format,args...){\
+	do{\
+		//\
+	}\
+while(0)
+		
 void log_warning();
 void log_error();
 void log_fatal();
