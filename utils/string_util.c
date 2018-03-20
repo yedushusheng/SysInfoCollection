d987fb5fc0f346f449872e4c50774337f27c6988
yedushusheng <yedushusheng@163.com>
2018-03-20 22:12:06 +0800

    增加字符串拷贝函数

--- utils/string_util.c
+++ utils/string_util.c
@@ -10,3 +10,13 @@ void check_null(p){
 int32_t str_to_int(char * str, int32_t base, int32_t num){
 
 }
+
+int32_t Strncpy(char * dst, char *src, int32_t max_len){
+	int32_t ret = RTCODE_SUCCESS;
+
+	check_null(str, RTCODE_ERROR);
+	strncpy(dst,src,max_len);
+	dst[max_len-1] = '\0';
+
+	return RTCODE_SUCCESS;
+}
--- utils/string_util.h
+++ utils/string_util.h
@@ -4,3 +4,4 @@
 void check_null(p);
 
 int32_t str_to_int(char *str,int32_t base,int32_t num);
+int32_t Strncpy(char *dst,char *src, int32_t max_len);
