31ed199a2c01a4f920ed7482fca86d7f5a7fb0dd
yedushusheng <yedushusheng@163.com>
2018-03-22 23:23:48 +0800

    实现字符删除空格函数

--- utils/string_util.c
+++ utils/string_util.c
@@ -20,3 +20,23 @@ int32_t Strncpy(char * dst, char *src, int32_t max_len){
 
 	return RTCODE_SUCCESS;
 }
+
+int32_t Str_trim(char * str){
+	int32_t ret = RTCODE_SUCCESS;
+
+	int32_t i,j,len;
+
+    i = strlen(str);
+	while(str[i] == ' '){
+		i--;
+		str[i] = '\0';
+	}
+
+	j = len -j;
+	while(str[j] == ' '){
+		j++;
+	}
+
+	j = i;
+	str[j-j] = '\0';
+}
