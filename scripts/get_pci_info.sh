afae743da045b65e306fa299a897f0aebdb0585c
yedushusheng <yedushusheng@163.com>
2018-04-01 23:18:10 +0800

    增加不同pic设备信息重定向输出

--- scripts/get_pci_info.sh
+++ scripts/get_pci_info.sh
@@ -1,4 +1,4 @@
 #/bin/bash
-lspci -nn | grep USB
-lspci -nn | grep Ether
-lspci -nn | grep VGA
+lspci -nn | grep USB > /opt/sys_info/pci_usb.info
+lspci -nn | grep Ether > /opt/sys_info/pci_eth.info
+lspci -nn | grep VGA > /opt/sys_info/pci_vga.info
